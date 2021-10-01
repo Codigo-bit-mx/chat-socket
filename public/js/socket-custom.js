0c8dbb3345ab7b6c760b52c924df4f79181cfacd
Ossmar <codigobitmx@gmail.com>
2021-10-01 13:59:28 -0500

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,48 @@
+var socket = io();
+
+let params = new URLSearchParams(window.location.search);
+
+if( !params.has('nombre') || !params.has('sala')) {
+    window.location = "index.html";
+    throw new Error ('El nombre y la sala son necesario');
+}
+let usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+    });
+});
+
+
+
+// escuchar
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+
+// Escuchar información con on
+socket.on('crearMensaje', function(mensaje) {
+    console.log('Servidor:', mensaje);
+});
+
+
+socket.on('listaPersonas', function(mensaje) {
+    console.log('si me escuchan:', mensaje);
+});
+
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('privado a:', mensaje);
+})
---
+++ server/utilidades/utilidad.js
@@ -0,0 +1,11 @@
+
+const crearUsuario = (nombre, mensaje) => {
+
+    return {
+        nombre,
+        mensaje,
+        fecha: new Date().getTime()
+    }
+}
+
+module.exports = {crearUsuario};
